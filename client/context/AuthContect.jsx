2d4a8b9616af30486a47c181724a3165b42a9a48
Skillyboy <fakoredeabbas@gmail.com>
2024-05-08 22:43:09 +0100

    connecting the frontend to the backend apis

--- client/context/AuthContect.jsx
+++ client/context/AuthContect.jsx
@@ -1,10 +1,15 @@
 import { createContext, useCallback, useState } from "react";
+import { baseUrl, postRequest } from "../src/utils/services";
 
 
 export const AuthContext = createContext()
 
 export const AuthContextProvider = ({children}) => {
     const [user, setUser] = useState(null)
+    const [registerError, setRegisterError] = useState(null)
+    const [isRegisterLoading, setIsRegisterLoading] = useState(false)
+    // const [registerInfo, setRegisterInfo] = useState()
+
     const [registerInfo, setRegisterInfo] = useState({
         username: "",
         email: "",
@@ -14,6 +19,23 @@ export const AuthContextProvider = ({children}) => {
     const updateRegisterInfo = useCallback((info) => {
         setRegisterInfo(info)
     }, [])
+
+    const registerUser = useCallback(async() => {
+        setIsRegisterLoading(true)
+        setRegisterError(null)
+
+        const response = await postRequest(`${baseUrl}/users/signup`, JSON.stringify(registerInfo))
+        
+        setIsRegisterLoading(false)
+        
+        if(response.error){
+            return setRegisterError(response)
+        }
+        localStorage.setItem("user", JSON.stringify(response))
+
+        setUser(response)
+    }, [])
+
     return <AuthContext.Provider value={{
         user,
         registerInfo,
